f4fa8d697bc663bed089746ca79a0418652d0cc1
SteveIris <45337252+SteveIris@users.noreply.github.com>
2019-10-31 15:35:19 +0300

    Фикс небольших проблем в проекте

--- PerfectCircleITProject/app/src/androidTest/java/com/example/asus/PerfectCircleITProject/ExampleInstrumentedTest.java
+++ PerfectCircleITProject/app/src/androidTest/java/com/example/asus/PerfectCircleITProject/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.asus.ivrfortheloveofgod", appContext.getPackageName());
+        assertEquals("com.example.asus.PerfectCircleITProject", appContext.getPackageName());
     }
 }
